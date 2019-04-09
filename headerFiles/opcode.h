4e1143f8e916473fd964a3c19143c6673d993d52
JebidiahCrumps <31321378+JebidiahCrumps@users.noreply.github.com>
2019-04-09 19:58:37 -0400

    Add files via upload

--- headerFiles/opcode.h
+++ headerFiles/opcode.h
@@ -4,7 +4,35 @@
 class opcode{
     public:
         //opcode();   
-        void ADC();
+    void ADC();
+
+    void LDA();	
+    void LDX();	
+    void LDY();
+    void LSR();
+    void NOP();
+    void ORA();
+    void PHA();	
+    void PHP();
+    void PLA();
+    void PLP();	
+    void ROL();
+    void ROR();
+    void RTI();
+    void RTS();
+    void SBC();
+    void SEC();
+    void SED();	
+    void SEI();
+    void STA();	
+    void STX();	
+    void STY();	
+    void TAX();	
+    void TAY();	
+    void TSX();	
+    void TXA();	
+    void TXS();
+    void TYA();	
 
 };
 #endif
