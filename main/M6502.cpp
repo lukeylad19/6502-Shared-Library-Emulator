9fc2002e032fe7462c3d2608099e08b591e7527a
JebidiahCrumps <31321378+JebidiahCrumps@users.noreply.github.com>
2019-04-13 23:39:03 -0400

    Add files via upload

--- main/M6502.cpp
+++ main/M6502.cpp
@@ -5,15 +5,15 @@
 #include "opcode.h"
 #include "memory.h"
 
-M6502_core::M6502_core(M6502_memory &mem){            //attach memory and reset
-    M = mem;
+M6502_core::M6502_core(M6502_memory mem){            //attach memory and reset
+    M = &mem;
     reset();
 }
 
 void M6502_core::stack_push(uint8_t byte){          //push to 6502's Stack
-    M.write(0x0100+sp,byte);
+    M->write(0x0100+SP,byte);
     if(SP== 0x00){
-        SP = 0xFF
+        SP = 0xFF;
     }
     else{
         SP--;
@@ -24,12 +24,12 @@ uint8_t M6502_core::stack_pop(){                    //pop from the 6502's stack
     if(SP == 0xFF){
         SP = 0x00;
     }
-    else sp++;
-    return M.read(0x0100 + SP);
+    else SP++;
+    return M->read(0x0100 + SP);
 }
 
 void M6502_core::run(){                              //execute 1 instruction
-    IR= M.read(PC++);                                //fetch instruction
+    IR= M->read(PC++);                                //fetch instruction
     execute(IR);
 }
 
@@ -39,7 +39,7 @@ void M6502_core::reset(){
     X=0x00;
     Y=0x00;
 
-    PC = (M.read(RESET_H)<<8) +M.read(RESET_L);     //reset the program counter
+    PC = (M->read(RESET_H)<<8) +M->read(RESET_L);     //reset the program counter
 
     SP = 0xFD;                                      //reset the stack pointer
     write_SR(0x02);                                 //reset the status register
@@ -48,21 +48,21 @@ void M6502_core::reset(){
 void M6502_core::irq(){
     if(!SR.I){                                          //check to see if interrupts are disabled
         SR.B = false;                                   //set break false
-        stack_push((pc >> 8)& 0xFF);                    //push upper byte of PC to stack
+        stack_push((PC >> 8)& 0xFF);                    //push upper byte of PC to stack
         stack_push(PC&0xFF);                            //push lower byte of PC to stack
-        stack_push(SR);                                 //push status register to stack
+        stack_push(read_SR());                                 //push status register to stack
         SR.I = true;                                    //disable interrupts
-        PC = ((M.read(IRQ_H)<<8)+M.read(IRQ_L));        //update PC with IRQ routine
+        PC = ((M->read(IRQ_H)<<8)+M->read(IRQ_L));        //update PC with IRQ routine
     }
 }
 
 void M6502_core::nmi(){
     SR.B = false;                                   //set break false
-    stack_push((pc >> 8)& 0xFF);                    //push upper byte of PC to stack
+    stack_push((PC >> 8)& 0xFF);                    //push upper byte of PC to stack
     stack_push(PC&0xFF);                            //push lower byte of PC to stack
-    stack_push(SR);                                 //push status register to stack
+    stack_push(read_SR());                                 //push status register to stack
     SR.I = true;                                    //disable interrupts
-    PC = ((M.read(NMI_H)<<8)+M.read(NMI_L));        //update PC with NMI routine
+    PC = ((M->read(NMI_H)<<8)+M->read(NMI_L));        //update PC with NMI routine
 }
 
 void M6502_core::execute(uint8_t val){
--- main/memory.cpp
+++ main/memory.cpp
@@ -1,9 +1,11 @@
 #include "memory.h"
+#include <stdint.h>
+#include <iostream>
 
-6502_memory::6502_memory(string romfile){
+M6502_memory::M6502_memory(std::string romfile){
     char* buffer;
     long size;
-    std::ifstream rom (romfile,std::ios:in|std::ios::binary|std::ios:ate);
+    std::ifstream rom (romfile,std::ios::in|std::ios::binary|std::ios::ate);
     if(rom.good()){
         size = rom.tellg();
         rom.seekg(0,std::ios::beg);
@@ -12,25 +14,26 @@
         rom.close();
     }
     else{
-        std::cout << "Error Opening File" << endl;
+        std::cout << "Error Opening File" << std::endl;
     }
 
     uint16_t start = (1 << 16)-size;
-    for(i=0;(start+i)<size;i++){
+    for(int i=0;(start+i)<size;i++){
         M[start+i] = buffer[i];
     }
 }
 
-uint8_t 6502_memory::read(uint16_t addr){
+uint8_t M6502_memory::read(uint16_t addr){
     return M[addr];
 }
 
-uint16_t 6502_memory::readWord(uint16_t addr){
+uint16_t M6502_memory::readWord(uint16_t addr){
     uint16_t temp = M[addr+1]<<8;
     temp = temp +M[addr];
+    return temp;
 }
 
-void 6520_memory::write(uint16_t addr, uint8_t byte){
+void M6502_memory::write(uint16_t addr, uint8_t byte){
     if(addr < (1<<16))
         M[addr] = byte;
 }
